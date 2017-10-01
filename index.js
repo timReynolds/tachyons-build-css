2ba1aede233e89095667e3e9456b999547fc127a
timReynolds <tim@timothyreynolds.co.uk>
2017-10-01 11:31:07 +0100

    Updated to use nextcss

--- index.js
+++ index.js
@@ -1,57 +1,58 @@
-'use strict'
+"use strict";
 
-const postcss = require('postcss')
-const cssnano = require('cssnano')
-const queries = require('css-mqpacker')
-const perfect = require('perfectionist')
-const prefixer = require('autoprefixer')
-const atImport = require('postcss-import')
-const media = require('postcss-custom-media')
-const vars = require('postcss-css-variables')
-const conditionals = require('postcss-conditionals')
-const rmComments = require('postcss-discard-comments')
-const classRepeat = require('postcss-class-repeat')
+const postcss = require("postcss");
+const cssnano = require("cssnano");
+const cssNext = require("postcss-cssnext");
+const queries = require("css-mqpacker");
+const perfect = require("perfectionist");
+const atImport = require("postcss-import");
+const conditionals = require("postcss-conditionals");
+const rmComments = require("postcss-discard-comments");
+const classRepeat = require("postcss-class-repeat");
 
-const getPlugins = function (options) {
-  options = options || {}
+const getPlugins = function(options) {
+  options = options || {};
 
   const perfectionistOptions = options.perfectionist || {
-    format: 'compact',
+    format: "compact",
     trimTrailingZeros: false
-  }
+  };
 
-  const atImportOptions = options.atImport || {}
+  const atImportOptions = options.atImport || {};
 
-  const plugins = [
-    atImport(atImportOptions), vars(), conditionals(), media(), queries(), perfect(perfectionistOptions), prefixer()
-  ]
+  const plugins = cssNext().plugins.concat([
+    atImport(atImportOptions),
+    conditionals(),
+    queries(),
+    perfect(perfectionistOptions)
+  ]);
 
   if (options.minify) {
-    plugins.push(cssnano())
-    plugins.push(rmComments())
+    plugins.push(cssnano());
+    plugins.push(rmComments());
   }
 
   if (options.repeat) {
-    let repeatNum = parseInt(options.repeat) || 4
+    let repeatNum = parseInt(options.repeat) || 4;
 
     if (repeatNum < 2) {
-      repeatNum = 4
+      repeatNum = 4;
     }
 
-    plugins.push(classRepeat({ repeat: repeatNum }))
+    plugins.push(classRepeat({ repeat: repeatNum }));
   }
 
   if (options.plugins) {
-    options.plugins.forEach(plugin => plugins.push(plugin))
+    options.plugins.forEach(plugin => plugins.push(plugin));
   }
 
-  return plugins
-}
+  return plugins;
+};
 
-module.exports = function tachyonsBuild (css, options) {
-  const plugins = getPlugins(options)
+module.exports = function tachyonsBuild(css, options) {
+  const plugins = getPlugins(options);
 
-  return postcss(plugins).process(css, options)
-}
+  return postcss(plugins).process(css, options);
+};
 
-module.exports.getPlugins = getPlugins
+module.exports.getPlugins = getPlugins;
